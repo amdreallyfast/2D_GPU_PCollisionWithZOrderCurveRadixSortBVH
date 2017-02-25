0d540434877762633ced5341ce15b81be523652f
amdreallyfast <amdreallyfast@hotmail.com>
2017-02-25 12:37:11 -0700

    starting to add files for the particle sorting, but I forgot to capitalize the first letters of my shaders, as is my coding standard
    And I haven't been doing it for shaders for some reason.  I did it in
    the render_particles_2D_CPU_GPU_hybrid_p_on_p_collisions, but not on
    this project's parent project.  Doing that now.

---
+++ Include/ComputeControllers/ParticleSort.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+// TODO: header
+class ComputeParticleSort
+{
+public:
+    ComputeParticleSort(unsigned int numParticles, const std::string &computeShaderKey);
+    ~ComputeParticleSort();
+
+
+
+private:
+
+};
